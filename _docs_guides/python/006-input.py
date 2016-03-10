25a3d4ba771839be0ac31d995f85780598f0d356
andfaulkner <andfaulkner@gmail.com>
2016-03-10 06:17:33 -0500

    a few more python tutorials

---
+++ _docs_guides/python/006-input.py
@@ -0,0 +1,10 @@
+# raw_input(varToAssignUserInputTo)
+## 		Read user input in from the terminal, assign it to a variable.
+
+# int(someStringThatCouldBeANum)
+## 		Return an integer object constructed from a number or string x, 
+##		or return 0 if no arguments are given
+
+x = int(raw_input("Please enter an integer: "))
+
+print x  	# outputs whatever you just inputted
---
+++ _docs_guides/python/008-functions.py
@@ -0,0 +1,15 @@
+def addthree(n):
+	return n + 3
+
+x = int(raw_input("Please enter an integer: "))
+
+print addthree(x)
+
+# this is OK too:
+# 		print addthree(int(raw_input("Please enter an integer: ")))
+
+
+### DEFAULT ARGUMENTS ###
+
+def addfive(n=3):
+	return n + 5
