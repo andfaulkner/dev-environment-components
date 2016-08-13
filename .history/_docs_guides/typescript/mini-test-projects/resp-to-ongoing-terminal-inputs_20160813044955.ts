706a7e06ba07fd21669baae39b685804b49fd12e
andfaulkner <andfaulkner@gmail.com>
2016-08-13 07:26:15 -0400

    added typescript cache to git ignore list

--- .history/_docs_guides/typescript/mini-test-projects/resp-to-ongoing-terminal-inputs_20160813044955.ts
+++
@@ -1,9 +0,0 @@
-var stdin = process.openStdin();
-
-stdin.addListener("data", function(d) {
-    // note:  d is an object, and when converted to a string it will
-    // end with a linefeed.  so we (rather crudely) account for that
-    // with toString() and then trim()
-    console.log("you entered: [" +
-        d.toString().trim() + "]");
-});
