2883cd12398be7574912e076329d980804964383
andfaulkner <andfaulkner@gmail.com>
2016-08-20 05:49:28 -0400

    improved react-typescript-webpack example proj

--- _docs_guides/typescript/react/app.tsx
+++ _docs_guides/typescript/react/app.tsx
@@ -2,23 +2,28 @@
 /// <reference path="../typings/react/react-dom.d.ts" />
 /// <reference path="../typings/app.d.ts" />
 
-import 'babel-polyfill';
-
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 export interface CommentProps {  }
+export interface CommentState {  }
 
-class CommentBox extends React.Component<CommentProps, {}> {
+console.log('js loaded');
+                                           //Props   //     State
+class CommentBox extends React.Component<CommentProps, CommentState> {
   render() {
     return (
       <div className="commentBox">
         Hello, world! I am a CommentBox.
-      </div>)
+      </div>);
   }
 };
 
-ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('content')
-);
+document.addEventListener("DOMContentLoaded", function(event) { 
+  console.log('DOM loaded - mounting React');
+
+  ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+  );
+});
