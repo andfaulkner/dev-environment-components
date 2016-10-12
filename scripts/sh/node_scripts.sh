7c38d9c70aca6d619005236674b5bc2b29aa3d0c
andfaulkner <andfaulkner@gmail.com>
2016-10-12 00:27:01 -0400

    new node aliases: clean, thorough node ghost process killer

--- scripts/sh/node_scripts.sh
+++ scripts/sh/node_scripts.sh
@@ -24,8 +24,10 @@ alias nrne="export LOG_LEVEL=error; npm run nodemon"
 
 alias ni="npm install"
 
-alias mds="script/devops --clean build development"
-
+############################### PROJECT CONVENIENCE FUNCTIONS ############################## 
+alias mds="script/devops build development --clean"
+alias nkn_PARTIAL='ps aux | grep "tsc\|node\|webpack\|nvm\|npm\|gulp\|grunt\|cordova\|express\|commander" | grep -v "grep.*\(\(tsc.*node.*\)\|\(node.*tsc\)\)*" | xargs kill -9; pm2 stop all'
+alias nkn='nkn_PARTIAL 2>/dev/null | grep -v "\(\(\Use \`pm2 show <id|name>\`.*details.*app\)\|\(┬─────┬\)\|\(┴─────┴\)\|\(App name.*mode.*cpu.*mem.*watching\)\)" | awk "{print $0}"'
 #alias mds="make dev-setup"
 #alias mdsr="make dev-setup; npm run nodemon"
 #alias mpt="make populate-translations"
