bc9653a24629889312f8d517256939d61635f581
Andrew Faulkner <andfaulkner@gmail.com>
2016-12-06 22:21:41 -0500

    added haskell docs section

---
+++ _docs_guides/_langs/haskell/0001-basics.hs
@@ -0,0 +1,29 @@
+main :: IO()
+
+------------------------------ Comments ------------------------------
+-- these are defined using -- followed by your comment
+
+------------------------------ Defining Variables ------------------------------
+-- typeless var definitions
+x = 3
+-- through type inference, Haskell automatically knows that the variable is of type Int
+
+-- typed var definitions
+y :: Int
+y = 3
+-- we've made explicit that y is an Int
+
+z :: Float
+z = 3
+
+lst = [2,3,5,7,11]
+
+total = sum (map (3*) lst)
+
+-- main must be defined in all files - an IO monad is always implicitly present (?)
+        -- main :: IO()
+main = print total
+
+
+mainer :: IO()
+mainer = print z
