362958df4bef3a39927bc78cc770ba7328609323
Andrew Faulkner <andfaulkner@gmail.com>
2018-09-26 04:06:35 -0400

    Fix headers in init-new-machine script

--- setup/init-new-machine.sh
+++ setup/init-new-machine.sh
@@ -1,11 +1,11 @@
 ############# INSTALLATIONS #############
-# Install Homebrew
+### Install Homebrew ###
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install neovim / nvim
+### Install neovim / nvim ###
 brew add neovim
 
-# Install CLI utils 
+### Install CLI utils ###
 brew add ack
 brew add tree
 brew add gettext
@@ -13,18 +13,18 @@ brew add moreutils   # Adds sponge, etc - see https://rentes.github.io/unix/util
 brew add readline
 brew add watchman
 
-# Install web utils
+### Install web utils ###
 brew install googler # Search google from the CLI
 brew add curl
 brew add wget
 
-# Install source control systems
+### Install source control systems ###
 brew add git
 brew add hub # Github extensions to git
 brew add subversion
 brew add maven
 
-# Install languages
+### Install languages ###
 brew add gradle
 brew add python
 brew add python3 # Also installs pip
@@ -33,33 +33,33 @@ brew add elixir
 brew add kotlin
 brew install sass/sass/sass
 
-# Install misc programming utils
+### Install misc programming utils ###
 brew add jq
 brew add llvm@4
 
-# Install package managers
+### Install package managers ###
 brew add leiningen
 brew add yarn
 
 #### Install DBs (databases) ####
-# postgres + helpers
+### postgres + helpers ###
 brew add postgresql
 brew add pgbadger
 brew add redis
 brew add sqlite
 brew add mongodb
 
-# Install completions
+### Install completions ###
 brew add pip-completion
 brew add gem-completion
 brew add bash-completion
 
-# Python setup installations 
+### Python setup installations  ###
 pip3 install --upgrade pip
 pip install --upgrade setuptools
 pip3 install --upgrade neovim # Get Neovim working with Python
 
-# Go setup installations
+### Go setup installations ###
 brew add go
 mkdir ~/projects
 mkdir $HOME/projects/go
@@ -68,18 +68,18 @@ echo 'export GOPATH=$HOME/projects/go' >> $HOME/.bash_profile
 echo 'export PATH=$PATH:$GOPATH/bin' >> $HOME/.bash_profile
 
 ##################### VERSION MANAGERS #####################
-# Install rbenv (ruby version manager)
+### Install rbenv (ruby version manager) ###
 brew add rbenv
 
-# Install nvm 
+### Install nvm ###
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
-# Install NodeJS - latest LTS version & set it as default version
+### Install NodeJS - latest LTS version & set it as default version ###
 nvm install --lts
 nvm ls | ack '\s+v' | tail -n1 | awk '{print $2}'
 
 ############# SETTINGS #############
-# Enable scrolling in less
+### Enable scrolling in less ###
 defaults write com.googlecode.iterm2 AlternateMouseScroll -bool true
 
 
