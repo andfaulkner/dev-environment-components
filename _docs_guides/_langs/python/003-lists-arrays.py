9c592d7b9abf2da80cf4f7a6abd1cf55820e5be1
Andrew Faulkner <andfaulkner@gmail.com>
2019-06-19 04:26:58 -0400

    Guide on Python arrays

---
+++ _docs_guides/_langs/python/003-lists-arrays.py
@@ -0,0 +1,17 @@
+
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+print("printing with a for loop")
+for x in mylist:
+    print(x)
+
+print("setup a list with multiple items defined simultaneously")
+mylist2 = [1, 2, 3, 4]
+print(mylist2)
+
